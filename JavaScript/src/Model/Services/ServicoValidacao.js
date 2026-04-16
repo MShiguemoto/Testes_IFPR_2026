ce0ec8328e0b8a8e955522c782001c74fcc67ecb
Mauricio Shiguemoto <shiguemoto.m@gmail.com>
2026-04-16 19:01:06 -0300

    test: Senha agora exige letra minúscula

--- JavaScript/src/Model/Services/ServicoValidacao.js
+++ JavaScript/src/Model/Services/ServicoValidacao.js
@@ -3,6 +3,7 @@ class ServicoValidacao {
     static validarSenha(senha){
         // Lógica de validação da senha
         if (senha.length < 8) return false;
+        if (!/[a-z]/.test(senha)) return false;
         return true;
     }
 }
