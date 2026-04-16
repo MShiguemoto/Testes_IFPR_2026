e7816cfae153904a7f9454ef992aad6227031d17
Mauricio Shiguemoto <shiguemoto.m@gmail.com>
2026-04-16 19:07:54 -0300

    test: Senhas necessitam de caractere especial

--- JavaScript/src/Model/Services/ServicoValidacao.js
+++ JavaScript/src/Model/Services/ServicoValidacao.js
@@ -6,7 +6,7 @@ class ServicoValidacao {
         if (!/[a-z]/.test(senha)) return false;
         if (!/[A-Z]/.test(senha)) return false;
         if (!/[\d]/.test(senha)) return false;
-
+        if (!/[!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]/.test(senha)) return false
 
         return true;
     }
